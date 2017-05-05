ce1bc0241d017241346482c1f578beed05b31ee3
linwalker <linyuhua@linyuhuadeMacBook-Air.local>
2017-05-05 10:23:28 +0800

    git config email

--- index.js
+++ index.js
@@ -4,8 +4,23 @@
 const Koa = require('koa');
 const app = new Koa();
 
+
+
+app.use( async( ctx, next) => {
+    const start = new Date();
+    await next();
+    const ms = new Date() - start;
+    ctx.set('X-Response-Time',`${ms}ms`);
+})
+
+app.use( async (ctx, next) => {
+    const start = new Date();
+    await next();
+    const ms = new Date() - start;
+    console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+})
+
 app.use( ctx => {
     ctx.body = 'node-login';
 })
-
 app.listen(3003);
