4483e35790236d31ee0311a8882e9e9cd8ad664e
linwaler <wzamate@163.com>
2017-06-07 22:30:49 +0800

    add callback without res handle

--- config.js
+++ config.js
@@ -5,5 +5,6 @@ module.exports = {
     'secrect': 'linwalkernodelogindemo',
     'database': 'mongodb://localhost:27017/node-login',
     'client_id': '4179c1e52309f6f55a65',
+    'client_secret': 'b3d249c905f6e2cb58a2921441c1aedd63831871',
     'scope': ['user:email'],
 };
